1cf28e2c64dd3708645c8f7b1756744b3afd819a
Nick <n.battista180@gmail.com>
2024-06-19 16:02:17 -0600

    force-dynamic

--- app/api/console/route.ts
+++ app/api/console/route.ts
@@ -1,6 +1,9 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
+// Opt out of caching for all data requests in the route segment
+export const dynamic = 'force-dynamic';
+
 // GET query all guest data for console
 export async function GET() {
   try {
