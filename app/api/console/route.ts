47de36e6a5ddc70a69083b769b9e25166140bdca
Nick Battista <55067567+Battistanic28@users.noreply.github.com>
2024-06-19 14:58:16 -0600

    add endpoint to surface all guest data (#12)

--- app/api/console/route.ts
+++ app/api/console/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
     const result = await sql`
               SELECT *
               FROM guests;`;
-    return NextResponse.json({ result }, { status: 200 });
+    return NextResponse.json(result.rows, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
