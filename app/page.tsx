326d37494806b2be6800cb0c9daa2f52e5936dbc
Nick <n.battista180@gmail.com>
2024-06-22 13:08:05 -0600

    add styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Page from '@/components/Page';
 import { Box, Typography } from '@mui/material';
 import config from '../config/config.json';
+import ImageTile from '@/components/ImageTile';
 
 export default function ResourcePage() {
   const { title, description } = config.HOME;
@@ -8,6 +9,7 @@ export default function ResourcePage() {
     <Page title={title} description={description}>
       <Box>
         <Typography>Add stuff here</Typography>
+        <ImageTile />
       </Box>
     </Page>
   );
---
+++ components/ImageTile.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+
+const ImageTile = () => {
+  return <Image alt="pop" src="/pop.png" width={700} height={650} />;
+};
+
+export default ImageTile;
