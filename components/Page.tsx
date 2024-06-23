4d6be0230e0a38ae53aec1f4ae12404e9df17570
Nick Battista <55067567+Battistanic28@users.noreply.github.com>
2024-06-23 09:37:58 -0600

    Styling improvements (#17)
    * add styling
    
    * fix hydration errors

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useState, useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
 import NavBar from './NavBar';
 import PasswordForm from './PasswordForm';
@@ -13,36 +13,41 @@ interface PageProps {
 }
 
 const Page: React.FC<PageProps> = ({ children, title, description }) => {
-  let initialAuthValue = false;
-  if (typeof window !== 'undefined') {
-    initialAuthValue = Boolean(localStorage.getItem('secret'));
-  }
   const [isPassowordErrored, setIsPasswordErrored] = useState<boolean>(false);
-  const [isAuthorized, setIsAuthorized] = useState<boolean>(initialAuthValue);
+  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    // isMounted approach is a work around for hydration errors - consider other solutions
+    setIsAuthorized(Boolean(localStorage.getItem('secret')));
+    setIsMounted(true);
+  }, []);
 
   return (
-    <Box sx={PageBody}>
-      {isAuthorized ? (
-        <>
-          <NavBar />
-          <Box sx={PageContent}>
-            <Typography variant="h2">{title}</Typography>
-            <Typography variant="body2">{description}</Typography>
-            <Box>{children}</Box>
-          </Box>
-        </>
-      ) : (
-        <>
-          <PasswordForm
-            setIsAuthorized={setIsAuthorized}
-            setIsPasswordErrored={setIsPasswordErrored}
-          />
-          {isPassowordErrored && (
-            <Typography>The password you entered is incorrect</Typography>
-          )}
-        </>
-      )}
-    </Box>
+    isMounted && (
+      <Box sx={PageBody}>
+        {isAuthorized ? (
+          <>
+            <NavBar />
+            <Box sx={PageContent}>
+              <Typography variant="h2">{title}</Typography>
+              <Typography variant="body2">{description}</Typography>
+              <Box>{children}</Box>
+            </Box>
+          </>
+        ) : (
+          <>
+            <PasswordForm
+              setIsAuthorized={setIsAuthorized}
+              setIsPasswordErrored={setIsPasswordErrored}
+            />
+            {isPassowordErrored && (
+              <Typography>The password you entered is incorrect</Typography>
+            )}
+          </>
+        )}
+      </Box>
+    )
   );
 };
 
